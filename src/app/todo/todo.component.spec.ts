789947beb122e306e0fb3a10a27ba47a35531fdd
Alexander May <alexander.may@codecentric.de>
2024-10-07 23:45:41 +0200

    add async to testing functions

--- src/app/todo/todo.component.spec.ts
+++ src/app/todo/todo.component.spec.ts
@@ -3,9 +3,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TodoComponent } from './todo.component';
 
 describe('TodoComponent', () => {
-  it('should add a todo', () => {});
+  it('should add a todo', async () => {});
 
-  it('should clear the input after adding a todo', () => {});
+  it('should clear the input after adding a todo', async () => {});
 
-  it('should remove a todo', () => {});
+  it('should remove a todo', async () => {});
 });
