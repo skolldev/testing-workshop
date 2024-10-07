81f4e6e1a44a026df926d4597010f62fa8dd03c8
Alexander May <alexander.may@codecentric.de>
2024-10-07 23:44:22 +0200

    add solution

--- src/app/todo/todo.component.spec.ts
+++ src/app/todo/todo.component.spec.ts
@@ -1,11 +1,43 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TodoComponent } from './todo.component';
+import { render, screen } from '@testing-library/angular';
+import userEvent from '@testing-library/user-event';
 
 describe('TodoComponent', () => {
-  it('should add a todo', () => {});
+  it('should add a todo', async () => {
+    await render(TodoComponent);
+    const user = userEvent.setup();
 
-  it('should clear the input after adding a todo', () => {});
+    const input = screen.getByRole('textbox', { name: 'Add your Todo' });
 
-  it('should remove a todo', () => {});
+    await user.type(input, 'Buy milk');
+    await user.click(screen.getByRole('button', { name: 'Add' }));
+
+    expect(screen.getByText('Buy milk')).toBeInTheDocument();
+  });
+
+  it('should clear the input after adding a todo', async () => {
+    await render(TodoComponent);
+    const user = userEvent.setup();
+
+    const input = screen.getByRole('textbox', { name: 'Add your Todo' });
+
+    await user.type(input, 'Buy milk');
+    await user.click(screen.getByRole('button', { name: 'Add' }));
+
+    expect(input).toHaveValue('');
+  });
+
+  it('should remove a todo', async () => {
+    await render(TodoComponent);
+    const user = userEvent.setup();
+
+    const input = screen.getByRole('textbox', { name: 'Add your Todo' });
+
+    await user.type(input, 'Buy milk');
+    await user.click(screen.getByRole('button', { name: 'Add' }));
+
+    await user.click(screen.getByRole('button', { name: 'Remove Buy milk' }));
+
+    expect(screen.queryByText('Buy milk')).not.toBeInTheDocument();
+  });
 });
